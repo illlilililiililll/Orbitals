c246b014b47b20a97776279e7078d42b313fe39b
illlilililiililll <kcwrenew22c@gmail.com>
2025-03-31 22:58:09 +0900

    20250331 ~5g

--- Orbital.py
+++ Orbital.py
@@ -5,47 +5,84 @@ a0 = 1.0
 Z = 1.0
 
 def sigma(r):
-    return Z*r/a0
-
-class Orbital:
-    def __init__(self, r, y):
-        self.r = r
-        self.y = y
-
-        if ((y==Y.s) and (r not in [R._1s, R._2s, R._3s])) or ((r==R._2p or r==R._3p) and (y not in [Y.px, Y.py, Y.pz])) or ((r==R._3d) and (y not in [Y.dz2, Y.dxy, Y.dxz, Y.dyz, Y.dx2y2])):
-            raise ValueError("Invalid R and Y function combination")
+    return 2*Z*r/a0
 
 # R
 class R:
     @staticmethod
     def _1s(r, a0=a0, Z=Z):
-        return 2 * (Z / a0)**(3/2) * np.exp(-sigma(r))
+        return 2 * Z**(3/2) * np.exp(-sigma(r)/2)
 
     @staticmethod
     def _2s(r, a0=a0, Z=Z):
         s = sigma(r)
-        return (1 / (2 * np.sqrt(2))) * (Z / a0)**(3/2) * (2 - s) * np.exp(-s/2)
+        return (1 / (2 * np.sqrt(2))) * Z**(3/2) * (2 - s) * np.exp(-s/2)
+    
+    @staticmethod
+    def _2p(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1 / (2 * np.sqrt(6))) * Z**(3/2) * s * np.exp(-s/2)
 
     @staticmethod
     def _3s(r, a0=a0, Z=Z):
         s = sigma(r)
-        return (1 / (81 * np.sqrt(3))) * (Z / a0)**(3/2) * (27 - 18*s + 2*s**2) * np.exp(-s/3)
-
-    @staticmethod
-    def _2p(r, a0=a0, Z=Z):
-        s = sigma(r)
-        return (1 / (2 * np.sqrt(6))) * (Z / a0)**(3/2) * s * np.exp(-s/2)
+        return (1 / (9 * np.sqrt(3))) * Z**(3/2) * (6 - 6*s + s**2) * np.exp(-s/2)
 
     @staticmethod
     def _3p(r, a0=a0, Z=Z):
         s = sigma(r)
-        return (4 / (81 * np.sqrt(6))) * (Z / a0)**(3/2) * (6 - s) * s * np.exp(-s/3)
+        return (1 / (9 * np.sqrt(6))) * Z**(3/2) * (4 - s) * s * np.exp(-s/2)
 
     @staticmethod
     def _3d(r, a0=a0, Z=Z):
         s = sigma(r)
-        return (4 / (81 * np.sqrt(30))) * (Z / a0)**(3/2) * s**2 * np.exp(-s/3)
+        return (1 / (9 * np.sqrt(30))) * Z**(3/2) * s**2 * np.exp(-s/2)
+    
+    @staticmethod
+    def _4s(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/96) * Z**(3/2) * (24 - 36*s + 12*s**2 - s**3) * np.exp(-s/2)
+    
+    @staticmethod
+    def _4p(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(32 * np.sqrt(15))) * Z**(3/2) * (20 - 10*s + s**2) * s * np.exp(-s/2)
 
+    @staticmethod
+    def _4d(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(96 * np.sqrt(5))) * Z**(3/2) * (6 - s) * s**2 * np.exp(-s/2)
+
+    @staticmethod
+    def _4f(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(96 * np.sqrt(35))) * Z**(3/2) * s**3 * np.exp(-s/2)
+    
+    @staticmethod
+    def _5s(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(300 * np.sqrt(5))) * Z**(3/2) * (120 - 240*s + 120*s**2 - 20*s**3 + s**4) * np.exp(-s/2)
+    
+    @staticmethod
+    def _5p(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(150 * np.sqrt(30))) * Z**(3/2) * (120 - 90*s + 18*s**2 - s**3) * s * np.exp(-s/2)
+
+    @staticmethod
+    def _5d(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(150 * np.sqrt(70))) * Z**(3/2) * (42 - 14*s + s**2) * s**2 * np.exp(-s/2)
+
+    @staticmethod
+    def _5f(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(300 * np.sqrt(70))) * Z**(3/2) * (8 - s) * s**3 * np.exp(-s/2)
+
+    @staticmethod
+    def _5g(r, a0=a0, Z=Z):
+        s = sigma(r)
+        return (1/(900 * np.sqrt(70))) * Z**(3/2) * s**4 * np.exp(-s/2)
+        
 # Y
 class Y:
     @staticmethod
@@ -70,7 +107,7 @@ class Y:
 
     @staticmethod
     def dxy(theta, phi):
-        return np.sqrt(15 / (16 * np.pi)) * np.sin(theta)**2 * np.cos(2*phi)
+        return np.sqrt(15 / (16 * np.pi)) * np.sin(theta)**2 * np.sin(2*phi)
 
     @staticmethod
     def dxz(theta, phi):
@@ -83,3 +120,93 @@ class Y:
     @staticmethod
     def dx2y2(theta, phi):
         return np.sqrt(15 / (16 * np.pi)) * np.sin(theta)**2 * np.cos(2*phi)
+    
+    @staticmethod
+    def fz3(theta, phi):
+        return np.sqrt(7 / (16 * np.pi)) * np.cos(theta) * (5 * np.cos(theta)**2 - 3)
+    
+    @staticmethod
+    def fyz2(theta, phi):
+        return np.sqrt(21 / (8 * np.pi)) * np.sin(theta) * np.sin(phi) * (5*np.cos(theta)**2 - 1)
+    
+    @staticmethod
+    def fxz2(theta, phi):
+        return np.sqrt(21 / (8 * np.pi)) * np.sin(theta) * np.cos(phi) * (5*np.cos(theta)**2 - 1)
+    
+    @staticmethod
+    def fxyz(theta, phi):
+        return np.sqrt(105 / (4 * np.pi)) * np.sin(theta)**2 * np.cos(theta) * np.sin(phi) * np.cos(phi)
+    
+    @staticmethod
+    def fzx2y2(theta, phi):
+        return np.sqrt(105 / (16 * np.pi)) * np.cos(theta) * np.sin(theta)**2 * np.cos(2*phi)
+    
+    @staticmethod
+    def fy3x2y2(theta, phi):
+        return np.sqrt(35 / (4 * np.pi)) * np.sin(theta)**3 * np.sin(phi) * (3 * np.cos(phi)**2 - 1)
+    
+    @staticmethod
+    def fxx23y2(theta, phi):
+        return np.sqrt(35 / (4 * np.pi)) * np.sin(theta)**3 * np.cos(phi) * (np.cos(phi)**2 - 3 * np.sin(phi)**2)
+    
+    @staticmethod
+    def gz4(theta, phi):
+        return np.sqrt(9 / (64 * np.pi)) * (35 * np.cos(theta)**4 - 30 * np.cos(theta)**2 + 3)
+
+    @staticmethod
+    def gz3y(theta, phi):
+        return np.sqrt(45 / (8 * np.pi)) * np.sin(theta) * np.cos(theta) * (7 * np.cos(theta)**2 - 3) * np.sin(phi)
+
+    @staticmethod
+    def gz3x(theta, phi):
+        return np.sqrt(45 / (8 * np.pi)) * np.sin(theta) * np.cos(theta) * (7 * np.cos(theta)**2 - 3) * np.cos(phi)
+
+    @staticmethod
+    def gz2xy(theta, phi):
+        return np.sqrt(45 / (16 * np.pi)) * np.sin(theta)**2 * (7 * np.cos(theta)**2 - 1) * np.sin(2 * phi)
+
+    @staticmethod
+    def gz2x2y2(theta, phi):
+        return np.sqrt(45 / (16 * np.pi)) * np.sin(theta)**2 * (7 * np.cos(theta)**2 - 1) * np.cos(2 * phi)
+
+    @staticmethod
+    def gzy3(theta, phi):
+        return np.sqrt(315 / (8 * np.pi)) * np.sin(theta)**3 * np.cos(theta) * (3 * np.cos(phi)**2 - 1) * np.sin(phi)
+
+    @staticmethod
+    def gzx3(theta, phi):
+        return np.sqrt(315 / (8 * np.pi)) * np.sin(theta)**3 * np.cos(theta) * (np.cos(phi)**2 - 3 * np.sin(phi)**2) * np.cos(phi)
+
+    @staticmethod
+    def gxyx2y2(theta, phi):
+        return np.sqrt(315 / (64 * np.pi)) * np.sin(theta)**4 * np.sin(2 * phi) * (np.cos(phi)**2 - np.sin(phi)**2)
+
+    @staticmethod
+    def gx4y4(theta, phi):
+        return np.sqrt(315 / (64 * np.pi)) * np.sin(theta)**4 * (np.cos(phi)**4 + np.sin(phi)**4 - 6 * np.cos(phi)**2 * np.sin(phi)**2)
+        
+class Orbital:
+    combinations = {
+        R._1s: [Y.s],
+        R._2s: [Y.s],
+        R._3s: [Y.s],
+        R._2p: [Y.px, Y.py, Y.pz],
+        R._3p: [Y.px, Y.py, Y.pz],
+        R._3d: [Y.dz2, Y.dxy, Y.dxz, Y.dyz, Y.dx2y2],
+        R._4s: [Y.s],
+        R._4p: [Y.px, Y.py, Y.pz],
+        R._4d: [Y.dz2, Y.dxy, Y.dxz, Y.dyz, Y.dx2y2],
+        R._4f: [Y.fz3, Y.fyz2, Y.fxz2, Y.fxyz, Y.fzx2y2, Y.fy3x2y2, Y.fxx23y2],  # 확장 가능
+        R._5s: [Y.s],
+        R._5p: [Y.px, Y.py, Y.pz], 
+        R._5d: [Y.dz2, Y.dxy, Y.dxz, Y.dyz, Y.dx2y2],
+        R._5f: [Y.fz3, Y.fyz2, Y.fxz2, Y.fxyz, Y.fzx2y2, Y.fy3x2y2, Y.fxx23y2],  # 확장 가능
+        R._5g: [Y.gz4, Y.gz3y, Y.gz3x, Y.gz2xy, Y.gz2x2y2, Y.gzy3, Y.gzx3, Y.gxyx2y2, Y.gx4y4],  # 확장 가능
+    }
+
+    def __init__(self, r, y):
+        self.r = r
+        self.y = y
+
+        if y not in self.combinations.get(r, []):
+            raise ValueError("Invalid R and Y function combination")
--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import Orbital
 import streamlit as st
 
 st.sidebar.markdown("## Parameters")
-N = st.sidebar.number_input('Vertex', 1, 300, 50, 10)
-r = st.sidebar.slider('Range', 10, 50, 20, 10)
+N = st.sidebar.slider('Vertex', 20, 300, 20, 10)
+# r = 100 - st.sidebar.slider('Density', 0, 100, 10, 1)
 
 st.sidebar.markdown("--------")
 st.sidebar.markdown("## Orbital Settings")
-# n = st.sidebar.selectbox('$ n $', (1, 2, 3))
-n = st.sidebar.radio("$ n $", (1, 2, 3))
+n = st.sidebar.radio("$ n $", (1, 2, 3, 4, 5))
 
-l_options = ['0 ($s$)', '1 ($p$)', '2 ($d$)']
+l_options = ['0 ($s$)', '1 ($p$)', '2 ($d$)', '3 ($f$)', '4 ($g$)']
 # l = st.sidebar.selectbox('$ l $', p_[:n])
 l = st.sidebar.radio("$ l $", l_options[:n])
 l_ = int(l[0])
@@ -22,8 +21,18 @@ ml_map = {
     (2, 1): ["$2p_x$", "$2p_y$", "$2p_z$"],
     (3, 0): ["$3s$"],
     (3, 1): ["$3p_x$", "$3p_y$", "$3p_z$"],
-    (3, 2): ["$3d_{xy}$", "$3d_{yz}$", "$3d_{z^2}$", "$3d_{xz}$", "$3d_{x^2-y^2}$"]
+    (3, 2): ["$3d_{xy}$", "$3d_{yz}$", "$3d_{z^2}$", "$3d_{xz}$", "$3d_{x^2-y^2}$"],
+    (4, 0): ["$4s$"],
+    (4, 1): ["$4p_x$", "$4p_y$", "$4p_z$"],
+    (4, 2): ["$4d_{xy}$", "$4d_{yz}$", "$4d_{z^2}$", "$4d_{xz}$", "$4d_{x^2-y^2}$"],
+    (4, 3): ["$4f_{xyz}$", "$4f_{yz^2}$", "$4f_{xz^2}$", "$4f_{z^3}$", "$4f_{x(x^2-3y^2)}$", "$4f_{y(3x^2-y^2)}$", "$4f_{z(x^2-y^2)}$"],
+    (5, 0): ["$5s$"],
+    (5, 1): ["$5p_x$", "$5p_y$", "$5p_z$"],
+    (5, 2): ["$5d_{xy}$", "$5d_{yz}$", "$5d_{z^2}$", "$5d_{xz}$", "$5d_{x^2-y^2}$"],
+    (5, 3): ["$5f_{xyz}$", "$5f_{yz^2}$", "$5f_{xz^2}$", "$5f_{z^3}$", "$5f_{x(x^2-3y^2)}$", "$5f_{y(3x^2-y^2)}$", "$5f_{z(x^2-y^2)}$"],
+    (5, 4): ["$5g_{z^4}$", "$5g_{z^3y}$", "$5g_{z^3x}$", "$5g_{z^2xy}$", "$5g_{z^2(x^2-y^2)}$", "$5g_{zy^3}$", "$5g_{zx^3}$", "$5g_{xy(x^2-y^2)}$", "$5g_{x^4+y^4}$"]
 }
+
 ml_options = ml_map.get((n, l_), [])
 
 function_map = {
@@ -40,7 +49,48 @@ function_map = {
     "$3d_{yz}$": "dyz",
     "$3d_{z^2}$": "dz2",
     "$3d_{xz}$": "dxz",
-    "$3d_{x^2-y^2}$": "dx2y2"
+    "$3d_{x^2-y^2}$": "dx2y2", 
+    "$4s$": "s",
+    "$4p_x$": "px",
+    "$4p_y$": "py",
+    "$4p_z$": "pz",
+    "$4d_{xy}$": "dxy",
+    "$4d_{yz}$": "dyz",
+    "$4d_{z^2}$": "dz2",
+    "$4d_{xz}$": "dxz",
+    "$4d_{x^2-y^2}$": "dx2y2",
+    "$4f_{xyz}$": "fxyz",
+    "$4f_{yz^2}$": "fyz2",
+    "$4f_{xz^2}$": "fxz2",
+    "$4f_{z^3}$": "fz3",
+    "$4f_{x(x^2-3y^2)}$": "fxx23y2",
+    "$4f_{z(x^2-y^2)}$": "fzx2y2",
+    "$4f_{y(3x^2-y^2)}$": "fy3x2y2",
+    "$5s$": "s",
+    "$5p_x$": "px",
+    "$5p_y$": "py",
+    "$5p_z$": "pz",
+    "$5d_{xy}$": "dxy",
+    "$5d_{yz}$": "dyz",
+    "$5d_{z^2}$": "dz2",
+    "$5d_{xz}$": "dxz",
+    "$5d_{x^2-y^2}$": "dx2y2",
+    "$5f_{xyz}$": "fxyz",
+    "$5f_{yz^2}$": "fyz2",
+    "$5f_{xz^2}$": "fxz2",
+    "$5f_{z^3}$": "fz3",
+    "$5f_{x(x^2-3y^2)}$": "fxx23y2",
+    "$5f_{z(x^2-y^2)}$": "fzx2y2",
+    "$5f_{y(3x^2-y^2)}$": "fy3x2y2",
+    "$5g_{z^4}$": "gz4",
+    "$5g_{z^3y}$": "gz3y",
+    "$5g_{z^3x}$": "gz3x",
+    "$5g_{z^2xy}$": "gz2xy",
+    "$5g_{z^2(x^2-y^2)}$": "gz2x2y2",
+    "$5g_{zy^3}$": "gzy3",
+    "$5g_{zx^3}$": "gzx3",
+    "$5g_{xy(x^2-y^2)}$": "gxyx2y2",
+    "$5g_{x^4+y^4}$": "gx4y4"
 }
 
 m_l = st.sidebar.radio("$ m_l $", ml_options)
@@ -49,4 +99,4 @@ m_l = st.sidebar.radio("$ m_l $", ml_options)
 orbital = Orbital.Orbital(getattr(Orbital.R, f'_{n}{l_options[l_][-3]}'), getattr(Orbital.Y, f'{function_map[m_l]}'))
 
 st.title(f"{m_l} Orbital")
-st.plotly_chart(util.plot(orbital, N, r), use_container_width=True)
+st.plotly_chart(util.plot(orbital, N, max(n**2, 10)), use_container_width=True)
